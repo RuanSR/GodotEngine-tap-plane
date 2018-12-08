e18971faa0ccd9fb8f621adbb975018a45f54734
RuanSR <ruansilvarosa@gmail.com>
2018-12-07 23:28:22 -0300

    v0.3 - 1 Script do player implementado.

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,8 +1,22 @@
 extends RigidBody2D
 
+var game
+var inpulse_force = -500
 
 func _ready():
 	set_process_input(true)
+	set_linear_velocity(Vector2(0,0))
+	game = get_tree().get_current_scene()
 
 func _input(event):
-	pass
+	if event.is_action_pressed("ui_touch"):
+		if game.state == game.game_states.PLAYING:
+			tap()
+		elif game.state == game.game_states.WAITING:
+			print("ESPERANDO")
+		else:
+			print("GAME_OVER")
+
+func tap():
+	set_linear_velocity(Vector2(0,inpulse_force))
+
