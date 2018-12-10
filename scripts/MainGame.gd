68b82b16daa605ae9bbb540974c4bd6dc81bf3b8
RuanSR <ruansilvarosa@gmail.com>
2018-12-10 10:40:55 -0300

    v0.3.1 - Feito pequenos ajustes nas cenas ClassicPlayer e MainClassic, tambem nos scripts MainGame e Player.

--- scripts/MainGame.gd
+++ scripts/MainGame.gd
@@ -1,13 +1,24 @@
 extends Node2D
 
 onready var lbl_score = get_node("HUD/Score")
-var score = 0
+onready var player = get_node("Player")
+
 enum game_states {PLAYING, WAITING, STOPED}
-var state 
+
+var score = 0
+var state
+
+var player_pos = Vector2(310,1280/2)
 
 func _ready():
+	set_process_game()
+	player.set_pos(player_pos)
 	lbl_score.set_text(str(score))
 	state = game_states.WAITING
+
+func _input(event):
+	if event.is_action_pressed("ui_touch"):
+		state = game_states.PLAYING
 
 func count_score():
 	score += 1
@@ -15,3 +26,7 @@ func count_score():
 	
 func game_over():
 	state = game_states.STOPED
+
+func set_process_game():
+	set_process(false)
+	set_process_input(true)
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -5,8 +5,16 @@ var inpulse_force = -500
 
 func _ready():
 	set_process_input(true)
+	set_process(true)
 	set_linear_velocity(Vector2(0,0))
 	game = get_tree().get_current_scene()
+	get_node("Anim").play()
+
+func _process(delta):
+	if game.state == game.game_states.WAITING:
+		set_gravity_scale(0)
+	elif game.state == game.game_states.PLAYING:
+		set_gravity_scale(10)
 
 func _input(event):
 	if event.is_action_pressed("ui_touch"):
